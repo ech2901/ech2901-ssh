e33c36c71423bd1402d5f9d57716c0748fa53fb7
ech2901 <ellischarlan@gmail.com>
2022-09-10 17:50:18 -0400

    Initial TransportHandler class for a ssh server to handle connections.

---
+++ SSH_Core/Transport/__init__.py
@@ -0,0 +1,23 @@
+from socket import socket
+
+class TransportHandler(object):
+    def __init__(self, server, client: socket):
+
+        self.server = server
+        self.client = client
+
+        self.get_client_version()
+        self.exchange_protocols()
+
+
+    def get_client_version(self):
+        self.client_version = self.client.recv(255).decode()
+        self.client.send(self.server.version_exchange.encode())
+
+    def exchange_protocols(self):
+        self.client_protocols = self.client.recv(2048)
+
+    def __repr__(self):
+        out = f'Client Version: {self.client_version}'
+        out = f'{out}Client Protocols: {self.client_protocols}'
+        return out
