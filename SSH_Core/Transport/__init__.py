88ccc87298de86ff0c5a69db2b03cd38992142e7
ech2901 <ellischarlan@gmail.com>
2022-09-15 19:28:09 -0400

    added SSH_Core Transports TransportHandler getting a packet and storing extra bytes in a buffer

--- SSH_Core/Transport/__init__.py
+++ SSH_Core/Transport/__init__.py
@@ -1,19 +1,27 @@
 from socket import socket
+from .Packets import *
+
 
 class TransportHandler(object):
-    def __init__(self, server, client: socket):
+    def __init__(self, server: socket, client: socket):
 
         self.server = server
         self.client = client
 
         self.get_client_version()
-        self.exchange_protocols()
+
+        self.buffer = b''
 
 
     def get_client_version(self):
         # TODO: handle preamble comments
         self.client_version = self.client.recv(255).decode()
         self.client.send(self.server.version_exchange.encode())
+
+    def get_packet(self):
+        data = self.buffer + self.client.recv(2048)
+        packet, self.buffer = Packet.decode(data)
+        return packet
 
     def exchange_protocols(self):
         # TODO: Send server acceptable protocols
