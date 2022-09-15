446a6cd92d6612b6bdf3fa9a4341aa05babdaafb
ech2901 <ellischarlan@gmail.com>
2022-09-15 19:29:03 -0400

    Changed SSH_Core Transport Packet AlgoNegotiation to sue a TransportMessage for it's key_exchange_init instead of a byte for readability

--- SSH_Core/Transport/Packets/__init__.py
+++ SSH_Core/Transport/Packets/__init__.py
@@ -1,4 +1,6 @@
 from SSH_Core import Byte, Boolean, UInt32, UInt64, String, MPInt, NameList
+from SSH_Core.Numbers import TransportMessage
+
 from dataclasses import dataclass
 from random import randint
 from os import urandom
@@ -17,7 +19,6 @@ class Packet(object):
 
         output = dict()
         for key, data_type in cls.__annotations__.items():
-            print(key, data_type, data)
             if key == 'payload':
                 size = output['packet_len']-output['padding_len']-1
                 output[key], data = data_type.decode(data, size)
@@ -25,13 +26,13 @@ class Packet(object):
                 output[key], data = data_type.decode(data, int(output['padding_len']))
             else:
                 output[key], data = data_type.decode(data)
-        return cls(**output)
+        return cls(**output), data
 
 
 
 @dataclass
 class AlgoNegotiation(object):
-    SSH_MSG_KEXINIT: Byte[1]
+    key_exchange_init: TransportMessage
     cookie: Byte[16]
     kex_algorithms: NameList
     server_host_key_algorithms: NameList
