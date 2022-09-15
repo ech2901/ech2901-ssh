e09a506ae8d666b14a44d806178d54af52823a91
ech2901 <ellischarlan@gmail.com>
2022-09-15 13:15:45 -0400

    Setup decoding of binary packets to use annotations. Created AlgoNegotiation packet decoding using class annotations as well.

--- SSH_Core/Transport/Packets/__init__.py
+++ SSH_Core/Transport/Packets/__init__.py
@@ -7,21 +7,52 @@ from struct import unpack
 @dataclass
 class Packet(object):
     packet_len: UInt32
-    padding_len: Byte
+    padding_len: Byte[1]
     payload: Byte
     padding: Byte
-    mac: Byte
+    mac: Byte[0]
 
     @classmethod
-    def decode(cls, data: bytes, mac_len=0):
-        packet_length, data = UInt32.decode(data)
-        padding_length, data = Byte.decode(data, 1)
+    def decode(cls, data: bytes):
 
-        size = packet_length-padding_length-1
-        payload, data = Byte.decode(data, size)
-        random_padding, data = Byte.decode(data, int(padding_length))
-        mac, data = Byte.decode(data, mac_len)
+        output = dict()
+        for key, data_type in cls.__annotations__.items():
+            print(key, data_type, data)
+            if key == 'payload':
+                size = output['packet_len']-output['padding_len']-1
+                output[key], data = data_type.decode(data, size)
+            elif key == 'padding':
+                output[key], data = data_type.decode(data, int(output['padding_len']))
+            else:
+                output[key], data = data_type.decode(data)
+        return cls(**output)
 
-        return cls(packet_length, padding_length, payload, random_padding, mac), data
+
+
+@dataclass
+class AlgoNegotiation(object):
+    SSH_MSG_KEXINIT: Byte[1]
+    cookie: Byte[16]
+    kex_algorithms: NameList
+    server_host_key_algorithms: NameList
+    encryption_algorithms_client_to_server: NameList
+    encryption_algorithms_server_to_client: NameList
+    mac_algorithms_client_to_server: NameList
+    mac_algorithms_server_to_client: NameList
+    compression_algorithms_client_to_server: NameList
+    compression_algorithms_server_to_client: NameList
+    languages_client_to_server: NameList
+    languages_server_to_client: NameList
+    first_kex_packet_follows: Boolean
+    reserved: UInt32
+
+    @classmethod
+    def decode(cls, data: bytes):
+        output = dict()
+        for key, data_cls in cls.__annotations__.items():
+            output[key], data = data_cls.decode(data)
+        return cls(**output)
+
+
 
 
